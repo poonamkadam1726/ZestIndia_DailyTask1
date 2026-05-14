be70fecc71b4ec0726664ff84773150dfa26eade
poonamkadam1726 <poonamkadam1726@gmail.com>
2026-05-14 12:13:39 +0530

    Create models and add data connection

---
+++ Day2-EntityFrameworkCore/Models/MyDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Day2_EntityFrameworkCore.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
