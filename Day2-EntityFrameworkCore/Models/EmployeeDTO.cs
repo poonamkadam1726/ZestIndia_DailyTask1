1bb4b9f844d93e03707cf5c7ce3eca3c069e260c
poonamkadam1726 <poonamkadam1726@gmail.com>
2026-05-14 12:31:05 +0530

    Add EmployeeDto class

---
+++ Day2-EntityFrameworkCore/Models/EmployeeDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Day2_EntityFrameworkCore.Models
+{
+    public class EmployeeDTO
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string City { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
