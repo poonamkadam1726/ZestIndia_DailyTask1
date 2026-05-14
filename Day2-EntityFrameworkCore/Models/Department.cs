399a6d9df34cf71cf6d7bba2fc4a3a1fb7f3acbc
poonamkadam1726 <poonamkadam1726@gmail.com>
2026-05-14 11:52:14 +0530

    Add project files.

---
+++ Day2-EntityFrameworkCore/Models/Department.cs
@@ -0,0 +1,10 @@
+﻿namespace Day2_EntityFrameworkCore.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Employee> Employees { get; set; }
+
+    }
+}
---
+++ Day2-EntityFrameworkCore/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace Day2_EntityFrameworkCore.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string City { get; set; }
+
+        public int DepartmentId { get; set; } // Foreign key property
+        public Department Department { get; set; }
+    }
+}
